838de66ab0f061d943434837930a96304a13398e
arcnmx <arcnmx@users.noreply.github.com>
2019-06-24 17:30:27 -0700

    buildRustCrate: match buildRustPackage install logic

--- pkgs/build-support/rust/build-rust-crate/install-crate.nix
+++ pkgs/build-support/rust/build-rust-crate/install-crate.nix
@@ -1,30 +1,32 @@
 crateName: metadata:
 ''
   runHook preInstall
-  mkdir -p $out
+  mkdir -p $out/bin $out/lib
   if [[ -s target/env ]]; then
     cp target/env $out/env
   fi
   if [[ -s target/link.final ]]; then
-    mkdir -p $out/lib
     cp target/link.final $out/lib/link
   fi
-  if [[ "$(ls -A target/lib)" ]]; then
-    mkdir -p $out/lib
-    cp target/lib/* $out/lib #*/
-    for lib in $out/lib/*.so $out/lib/*.dylib; do #*/
-      ln -s $lib $(echo $lib | sed -e "s/-${metadata}//")
+  if [[ -d target/lib ]]; then
+    find target/lib \
+      -maxdepth 1 \
+      -regex ".*\.\(so.[0-9.]+\|so\|a\|dylib\|rlib\)" \
+      -print0 | xargs -r -0 cp -t $out/lib
+    for lib in $(find $out/lib -name '*-${metadata}*'); do #*/
+      ln -s $lib $(sed -e "s/-${metadata}//" <<< $lib)
     done
   fi
   if [[ "$(ls -A target/build)" ]]; then # */
-    mkdir -p $out/lib
     cp -r target/build/* $out/lib # */
   fi
   if [[ -d target/bin ]]; then
-    if [[ "$(ls -A target/bin)" ]]; then
-      mkdir -p $out/bin
-      cp -P target/bin/* $out/bin # */
-    fi
+    find target/bin \
+      -maxdepth 1 \
+      -type f \
+      -executable ! \( -regex ".*\.\(so.[0-9.]+\|so\|a\|dylib\)" \) \
+      -print0 | xargs -r -0 cp -t $out/bin
   fi
+  rmdir --ignore-fail-on-non-empty $out/lib $out/bin
   runHook postInstall
 ''
